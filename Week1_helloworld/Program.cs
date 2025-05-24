b23ec41001ad02a7ca8c16c16e645703eb1f0b31
liu-jiahe08 <2270731472@qq.com>
2025-05-24 20:06:12 +0800

    test

---
+++ Week1_helloworld/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Week1_helloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("你好！我是刘佳禾");
+        }
+    }
+}
